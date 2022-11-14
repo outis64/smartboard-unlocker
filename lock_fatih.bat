a31e0b2ecda77f19df6c44a0c06b651b7e8f11ce
outis sixty-four <99974466+outis64@users.noreply.github.com>
2022-11-14 23:46:54 +0300

    Add files via upload

---
+++ lock_fatih.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+color b
+taskkill /f /im servisatk.exe                                                                                                                                                                                       
+taskkill /f /im explorer.exe
+start explorer.exe
+msg *done! - github/outis64*
+exit
---
+++ unlock_fatih.bat
@@ -0,0 +1,6 @@
+@echo off
+color b
+echo opened atak services.                                                                                                                                                                                    
+start servisatk.exe
+msg *done! - github/outis64*
+exit
